bc509b9854d05e6d1049e6b8da6fdbbe73f05d30
Charles Ellis <hamled@hamled.name>
2016-12-12 19:57:57 -0800

    Custom screen-256color terminfo
    This is based on the fix suggested by Neovim for correcting an issue
    with the C-h key combination when using tmux. See
    https://github.com/neovim/neovim/wiki/FAQ#my-ctrl-h-mapping-doesnt-work
    for more details.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@
 git remote add -f Hamled-dotvim git@github.com:Hamled/dotvim.git
 git subtree add --squash --prefix dotvim Hamled-dotvim master
 git submodule init && git submodule update # .gitmodules should be kept current
+
+# Setup terminfo
+tic terminfo/screen-256color.ti
