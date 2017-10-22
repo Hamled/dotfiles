08e2e47cfa244ddadb9f5413d91173508d0d4626
Charles Ellis <hamled@hamled.name>
2017-10-21 23:53:42 -0700

    Refactor setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO: Setup dotfiles automatically
-
 # Setup dotvim
 git remote add -f Hamled-dotvim git@github.com:Hamled/dotvim.git
 git subtree add --squash --prefix dotvim Hamled-dotvim master
@@ -10,7 +8,15 @@ git submodule init && git submodule update # .gitmodules should be kept current
 # Setup terminfo
 tic terminfo/screen-256color.ti
 
+if [ -x "$(command -v realpath)" ]; then
+  DOTFILES="$(realpath --relative-to=$HOME $(pwd))"
+else
+  DOTFILES="$(pwd)"
+fi
+
 # Setup Tmux Plugin Manager
 pushd ~
-ln -s projects/dotfiles/.tmux
+ln -s "$DOTFILES/.tmux"
 popd
+
+# TODO: Setup other dotfiles automatically
